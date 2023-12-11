9eb5f9a57446ff4255bd27ecc67e7fce337ada7a
luotg <133379821+luotg@users.noreply.github.com>
2023-12-10 18:51:31 -0600

    Update ucollege.js

--- js/ucollege.js
+++ js/ucollege.js
@@ -5,12 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             const main = document.querySelector('main');
             
-            // Loop through sections in JSON
             data.sections.forEach(section => {
                 const sectionElement = document.createElement('section');
                 sectionElement.innerHTML = `<h1>${section.title}</h1>`;
                 
-                // Loop through content in each section
                 section.content.forEach(paragraph => {
                     sectionElement.innerHTML += `<p>${paragraph}</p>`;
                 });
